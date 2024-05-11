e5cc0bb3f492f14d53e18c98168cf88123447df3
Phạm Quang Trường An <139692078+DevelopPlus2024@users.noreply.github.com>
2024-05-11 22:11:36 +0700

    cc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Đợi cho trang web tải hoàn toàn xong trước khi chuyển hướng
 window.onload = function() {
     // Thay đổi URL tới trang mới
-    window.location.href = "http://rvs34rl34g.loclx.io/";
+    window.location.href = "http://qutr6qtqzx.loclx.io/";
 };
 
