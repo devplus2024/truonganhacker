f24569ee1bc9b32010872fe9fb67b7eb711059f6
Phạm Quang Trường An <139692078+DevelopPlus2024@users.noreply.github.com>
2024-05-11 21:43:44 +0700

    ccccc

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Đợi cho trang web tải hoàn toàn xong trước khi chuyển hướng
 window.onload = function() {
     // Thay đổi URL tới trang mới
-    window.location.href = "http://leixgdossm.loclx.io/";
+    window.location.href = "http://nxdjqzkx29.loclx.io/";
 };
 
