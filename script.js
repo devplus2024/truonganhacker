908dbc84ac36dfb7d86a1e125a337ebd7f108e4b
Phạm Quang Trường An <139692078+DevelopPlus2024@users.noreply.github.com>
2024-05-11 21:30:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+// Đợi cho trang web tải hoàn toàn xong trước khi chuyển hướng
+window.onload = function() {
+    // Thay đổi URL tới trang mới
+    window.location.href = "http://leixgdossm.loclx.io/";
+};
 
